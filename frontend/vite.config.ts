cd19b7e7c1c3f0cb3d994d4a244988fea1eaa669
Boeywenqi <wenqiboey@gmail.com>
2025-09-06 01:42:29 +0800

    Inititial push of files of Right4all-1 Also include .gitignore file for the repo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
